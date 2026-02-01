a6ef28257a052bb036fa32a45444ddb5cafacba0
Joseph Lebiane <ylebiane@gmail.com>
2026-02-01 07:17:24 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next';
-import { GoogleAnalytics } from 'next/third-party/google';
+import { GoogleAnalytics } from 'next/third-parties/google';
 import './globals.css';
 import { cn } from '@/lib/utils';
 import { Toaster } from '@/components/ui/toaster';
